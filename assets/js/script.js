4d42337b5b9b226b86a72243ea5b98c3776b0180
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2021-06-25 22:39:05 -0500

    figured out how to completely generate dynamic list items with no limit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,33 @@
+consoleTextInput = document.querySelector("#console");
 
+testArrayOfStuff = ['apples', 'reaching out', 'touching me', `good times never seem so good`, `im feeling sad`, `to believe there never was oh no no`];
+
+function logkey(event) {
+    console.log(event.key.toUpperCase());
+
+    if (event.key == "h") {
+        testArrayOfStuff.shift();
+        consoleTextInput.children[0].innerHTML = "";
+        drawAllItems();
+    }
+}
+
+function drawAllItems() {
+    for (let index = 0; index < testArrayOfStuff.length; index++) {
+        var listItem = document.createElement("li");
+        listItem.textContent = testArrayOfStuff[index];
+        consoleTextInput.children[0].appendChild(listItem);
+        listItem.setAttribute("style", " color:white; background: #777777; padding: 5px; margin-left: 35px;");
+    }
+}
+
+function init(){
+    drawAllItems();
+}
+
+document.addEventListener("keydown", logkey);
+
+init();
 // 1. take input
 // 2. check if correct syntax
 // 3. 
