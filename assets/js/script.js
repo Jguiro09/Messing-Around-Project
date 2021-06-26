bd7ad3c4a96e1112e7c25a2140a86ba0fb8d6eee
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2021-06-26 01:47:13 -0500

    working console message sending #10

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 consoleTextInput = document.querySelector("#console");
 logOfPreviousMsg = [`Person@Persons-PC MINGW64 ~/Documents/git-test-repo (testbranch)`, `$ git add .`, `Person@Persons-PC MINGW64 ~/Document/git-test-repo (testbranch)`, `$ git commit -m "finally added a div in my div"`, `Person@Persons-PC MINGW64 ~/Document/git-test-repo (testbranch)`, `$ git push -u origin personalBranch`]
 approvedCharacters = [8, 13, 27, 32, 186];
+liItemStyling = `color:white; background: #777777; padding: 5px;`;
+
+/*
+add Person@Persons-PC MINGW64 ~/Document/git-test-repo (testbranch) every enter
+*/
 
 function removeTopItemInList(){
     logOfPreviousMsg.shift();
@@ -10,35 +15,47 @@ function removeTopItemInList(){
 function addBottomItemInList(newStr){
     if(typeof newStr != "string");
     logOfPreviousMsg = logOfPreviousMsg.concat(newStr);
+    drawAllItems();
 }
 
 
 function keyDownEvent(event) {
     if(!((47 < event.keyCode && event.keyCode <= 90) || approvedCharacters.includes(event.keyCode))){
-        console.log(event.keyCode, event.key);
         return;
     }
+
+    if(event.key === `Backspace`){  //backspace
+        var tempTextContent = consoleTextInput.querySelector("#textbox").textContent; //get current bottom row text content
+        consoleTextInput.querySelector("#textbox").textContent = tempTextContent.substring(0, tempTextContent.length - 1); //add current bottom text with new character choice
+        return
+    }
+
+    if(event.key === `Enter`){ //enter
+        var tempTextContent = consoleTextInput.querySelector("#textbox").textContent;
+        addBottomItemInList(`Person@Persons-PC MINGW64 ~/Documents/git-test-repo (testbranch)`);
+        addBottomItemInList(tempTextContent);
+        removeTopItemInList();
+        removeTopItemInList();
+        return
+    }
+
+    //add on any other types of characters
     var tempTextContent = consoleTextInput.querySelector("#textbox").textContent; //get current bottom row text content
     consoleTextInput.querySelector("#textbox").textContent = tempTextContent + event.key; //add current bottom text with new character choice
-
-    if (event.key == "ENTER") {
-        testArrayOfStuff.shift();
-        consoleTextInput.children[0].innerHTML = "";
-        drawAllItems();
-    }
 }
 
 function drawAllItems() {
+    consoleTextInput.children[0].innerHTML = '';
     for (let index = 0; index < logOfPreviousMsg.length; index++) {
         var listItem = document.createElement("li");
         listItem.textContent = logOfPreviousMsg[index];
         consoleTextInput.children[0].appendChild(listItem);
-        listItem.setAttribute("style", " color:white; background: #777777; padding: 5px;");
+        listItem.setAttribute("style", liItemStyling);
     }
     var listItem = document.createElement("li");
     listItem.textContent = `$ `;
     consoleTextInput.children[0].appendChild(listItem);
-    listItem.setAttribute("style", " color:white; background: #777777; padding: 5px;");
+    listItem.setAttribute("style", liItemStyling);
     listItem.setAttribute("id", "textbox")
 }
 
