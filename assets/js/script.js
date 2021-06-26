4575e8e47838002f5090afc15ceca81e3f4dabba
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2021-06-25 22:04:50 -0500

    this is a fix for #5

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
-console.log("poergjioeroigjeaijor");
 
-
-var time = 100;
-
-var timer = setInterval(function ()
-{
-    time--;
-    console.log(time);
-},1000);
+// 1. take input
+// 2. check if correct syntax
+// 3. 
+// 4. box with multiple lines
+// 5. delete top row
+// 6. move text from one row to the one above it when enter is hit
+// 7.
+// 8.
+// 9.
---
+++ todo.md
@@ -0,0 +1,11 @@
+### javascript stuff
+- working console
+
+### html todo
+- aside element that reads json files for other sites to go to
+
+### css todo
+- good color format
+
+### readme todo
+- add reflog to roll back commits
